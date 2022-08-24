62250d691e38a51572d2e8b59227f4c226f0fbbc
mirzabakti <mirza.bakti@gmail.com>
2022-08-25 01:02:41 +0700

    auth, login register

--- components/Banner.js
+++ components/Banner.js
@@ -7,10 +7,10 @@ const Banner = () => {
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 container mx-auto p-5">
         <Carousel>
           <div className="relative h-32 lg:h-96">
-            <Image src="/images/banner1.png" alt="..." width={100} height={100} objectFit="cover" layout="fill" />
+            <Image src="/images/banner1.png" alt="..." objectFit="cover" layout="fill" />
           </div>
           <div className="relative h-32 lg:h-96">
-            <Image src="/images/banner2.png" alt="..." width={100} height={100} objectFit="cover" layout="fill" />
+            <Image src="/images/banner2.png" alt="..." objectFit="cover" layout="fill" />
           </div>
         </Carousel>
       </div>
